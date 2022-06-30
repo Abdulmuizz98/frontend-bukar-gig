ff4d2af1fdd823195ef9938bc163c4ecc45faa0c
Hamzat Abdulmuizz O <75758703+Abdulmuizz98@users.noreply.github.com>
2022-06-30 20:01:58 +0100

    more to drivers page

--- src/pages/Drivers/CreateDriver.js
+++ src/pages/Drivers/CreateDriver.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import './css/CreateDriver.css'
 import CreateDriverForm from '../../components/create-driver/CreateDriverForm'
+import Permissions from '../../components/create-driver/Permissions'
+import UserPassword from '../../components/create-driver/UserPassword'
+import PaymentDetails from '../../components/create-driver/PaymentDetails'
+import AddressDetails from '../../components/create-driver/AddressDetails'
 
 function CreateDriver() {
     return (
         <div className="CreateDriver">
             <CreateDriverForm/>
+            <PaymentDetails/>
+            <AddressDetails/>
+            <Permissions/>
+            <UserPassword/>
         </div>
     )
 }
