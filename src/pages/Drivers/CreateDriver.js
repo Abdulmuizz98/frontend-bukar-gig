a7519fd1bf5a1fd8950c5aa9bb4c2c948b6384dc
Hamzat Abdulmuizz O <75758703+Abdulmuizz98@users.noreply.github.com>
2022-07-02 02:22:11 +0100

    permissions added buttom buttons

--- src/pages/Drivers/CreateDriver.js
+++ src/pages/Drivers/CreateDriver.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './css/CreateDriver.css'
+import Button from '../../components/mini/Button'
 import CreateDriverForm from '../../components/create-driver/CreateDriverForm'
 import Permissions from '../../components/create-driver/Permissions'
 import UserPassword from '../../components/create-driver/UserPassword'
@@ -14,6 +15,13 @@ function CreateDriver() {
             <AddressDetails/>
             <Permissions/>
             <UserPassword/>
+            <div className='buttons-main'>
+                <div className='right'>
+                <Button className='cancel' label={"Cancel"} color={"#868686"} borderColor={"#868686"} />
+                <Button className='submit' margin={"0px 0px 0px 20px"} color={"var(--white)"} background={"var(--blue)"} label={"Save"} />
+                </div>
+            </div>
+
         </div>
     )
 }
